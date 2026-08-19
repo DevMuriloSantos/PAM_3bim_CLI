a50001c63f1a3cb7de9d8d9403610f96505f6add
Murilo Santos <murilopaulo051@gmail.com>
2026-08-18 22:38:31 -0300

    feat: criação do novo projeto react-native

---
+++ App.tsx
@@ -0,0 +1,20 @@
+import { StyleSheet } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import Home from './components/Home';
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Home />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
